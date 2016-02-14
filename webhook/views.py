14734c06144c07349cafaa395660f719168f821a
Travis Eden <i@traviseden.com>
2016-02-13 16:56:26 -0800

    webhook returns message in payload

--- webhook/views.py
+++ webhook/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from rest_framework.decorators import parser_classes
 
-
-def webhook(request):
-    return HttpResponse("Hai!")
+@parser_classes((JSONParser,))
+def webhook(request, format=None):
+    return HttpResponse(request.data['message'])
