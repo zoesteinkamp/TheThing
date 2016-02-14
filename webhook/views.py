19d3586e24232ac4ab5a1fcbd8f430b566e8e9ff
Travis Eden <i@traviseden.com>
2016-02-13 17:09:25 -0800

    fix response

--- webhook/views.py
+++ webhook/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from rest_framework.decorators import parser_classes
+from rest_framework.parsers import JSONParser
 
 @parser_classes((JSONParser,))
 def webhook(request, format=None):
-    return HttpResponse(request.data['message'])
+
+    if request.method == "POST":
+        data = JSONParser().parse(request)
+        return HttpResponse(data["message"])
